a563be3af476eb50c70eeba2faa2778883379245
admin <co.hss1011@gmail.com>
2024-10-18 07:08:40 +0900

    Initial commit

---
+++ src/test/java/com/hhplus/concertReservation/ConcertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.concertReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
